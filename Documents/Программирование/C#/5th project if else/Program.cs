c70dd1843d02f947f7eb903fba1a49217024442d
Rin <anastasiatimohina440@gmail.com>
2022-10-01 21:46:16 +0300

    add 5th project (if...else)

---
+++ Documents/Программирование/C#/5th project if else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "rin")
+{
+    Console.WriteLine("Hello, Rin, glad to see you");
+}
+else
+{
+    Console.Write("Go away, ");
+    Console.Write(username);
+}
